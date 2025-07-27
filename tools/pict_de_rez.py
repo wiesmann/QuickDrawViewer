c8e38c06ac6e5bac3165a68a0b7086936fa58f8a
Matthias Wiesmann <matthias.wiesmann@gmail.com>
2025-07-27 09:25:09 +0200

    Script now handles more complete resource headers.

--- tools/pict_de_rez.py
+++ tools/pict_de_rez.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 
@@ -6,7 +6,7 @@ import os
 import re
 import sys
 
-START_RE = r"data\s*'(....)'\s*\((\d+)\)\s*{"
+START_RE = r"data\s*'(....)'\s*\(([^)]+)\)\s*{"
 DATA_RE = r"\s*\$\"([0-9A-Z\s]+)\""
 END_RE = r"\s*};"
   
@@ -14,11 +14,18 @@ END_RE = r"\s*};"
 def derez_pict(f_handle, out_dir):
   file_type = None
   resource_number = None
+  resource_name = None
   data = b""
   for line in f_handle:
     match = re.match(START_RE, line) 
     if match:
-      file_type, resource_number = match.groups()
+      file_type, resource_descriptor = match.groups()
+      tags = resource_descriptor.split(',')
+      tags = tuple(map(lambda s : s.strip(), tags))
+      resource_number = tags[0]
+      for tag in tags[1:]:
+        if tag[0]=='"':
+          resource_name = tag.strip('"')
       continue
     match = re.match(DATA_RE, line)
     if match:
@@ -27,12 +34,16 @@ def derez_pict(f_handle, out_dir):
       continue
     match = re.match(END_RE, line)
     if match:
-      out_name = str(resource_number) + "." + file_type
+      if resource_name:
+        out_name = resource_name + '.' + file_type
+      else:
+        out_name = 'R' + str(resource_number) + '.' + file_type
       if file_type == 'PICT':
         data = b"\x00" * 512 + data
         with open(os.path.join(out_dir, out_name), 'wb') as out_handle:
           out_handle.write(data)
       data = b""
+      resource_name = None
 
 def main(argv):
   out_dir = os.getcwd()
